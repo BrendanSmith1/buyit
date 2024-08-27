3e1c9f2cc2737ebefaf547fdb532e0327b1a9e33
BrendanSmith <brendansmith464@gmail.com>
2024-08-27 12:32:24 -0400

    About page added

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    get "about", to: "pages#about"
+  end
 end
