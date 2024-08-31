1be1acd882cc03dd93ce1047692829d04dfc7878
BrendanSmith <brendansmith464@gmail.com>
2024-08-31 11:08:37 -0400

    Created index and show for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 Product.destroy_all
+puts 'Deleting previous products...'
 
+puts 'Creating 20 products...'
 20.times do
   Product.create!(title: Faker::Appliance.equipment, price: Faker::Number.decimal(l_digits: 2))
 end
+
+puts 'Finished!'
