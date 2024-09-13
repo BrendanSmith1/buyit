d390946564728c0031cedce419371fad6755a86d
BrendanSmith <brendansmith464@gmail.com>
2024-09-13 14:42:02 -0400

    Need a new attribute for product to be able to show available stock and leave overall stock quant unchanged until checkout

--- app/controllers/cartproducts_controller.rb
+++ app/controllers/cartproducts_controller.rb
@@ -9,19 +9,7 @@ class CartproductsController < ApplicationController
       @cart.user = current_user
     end
     @product = Product.find(params[:product_id])
-    @cart_product = @cart.cart_products.find_by(product_id: @product.id)
-
-    # We want to add the product to the cart
-    if @cart_product.present?
-      # If the product already exists in the cart, increase the quantity
-      @cart_product.update(quantity: (@cart_product.quantity + 1))
-    else
-      # Otherwise, create a new cart product
-      @cart_product = @cart.cart_products.create!(product: @product, quantity: 1)
-    end
-
-    # If the product is added to the cart, reduce the stock
-    @product.update(stock_quantity: (@product.stock_quantity - 1))
+    @product.add_to_cart(@cart, 1)
 
     # If the cart is saved, redirect the user to the cart show page
     if @cart.save
@@ -37,25 +25,16 @@ class CartproductsController < ApplicationController
     @cart_product = @cart.cart_products.find(params[:id])
     @prev_quantity = @cart_product.quantity
     @new_quantity = params[:cart_product][:quantity].to_i
-    @product = Product.find(@cart_product.product_id)
-    # debugger
 
     # If the quantity is > 0, update the quantity, otherwise destroy the cart product
-    if params[:cart_product][:quantity].to_i.positive?
-      @cart_product.update(quantity: params[:cart_product][:quantity])
-    else
-      @cart_product.destroy
-    end
+    @new_quantity.positive? ? @cart_product.update(quantity: params[:cart_product][:quantity]) : @cart_product.destroy
 
-    # If the quantity is increased (prev < new) reduce stock, if quantity is decreased (prev > new) increase stock
-    if @product.enough_stock?(@new_quantity)
-      if @prev_quantity < @new_quantity
-        @product.update(stock_quantity: (@product.stock_quantity + @prev_quantity - @new_quantity))
-      elsif @prev_quantity > @new_quantity
-        @product.update(stock_quantity: (@product.stock_quantity + @prev_quantity - @new_quantity))
-      else
-        # Do nothing
-      end
+    # If the stock is not enough, update the cart product quantity to the stock quantity
+    # The user can change the quant of stock!
+    # Need a new attribute to store the original quantity of the product, which only gets changed when the user checks out
+    if Product.find(@cart_product.product_id).enough_stock?(@new_quantity)
+      # If the quantity is increased (prev < new) reduce stock, if quantity is decreased (prev > new) increase stock
+      @new_quantity > @prev_quantity ? @product.decrease_stock!(@new_quantity - @prev_quantity) : @product.increase_stock!(@prev_quantity - @new_quantity)
     else
       # If the stock is not enough, update the cart product quantity to the stock quantity
       @cart_product.update(quantity: @product.stock_quantity)
--- app/models/product.rb
+++ app/models/product.rb
@@ -10,20 +10,23 @@ class Product < ApplicationRecord
   end
 
   def decrease_stock!(quantity)
-    update!(stock: stock - quantity)
+    update!(stock_quantity: stock_quantity - quantity)
   end
 
   def increase_stock!(quantity)
-    update!(stock: stock + quantity)
+    update!(stock_quantity: stock_quantity + quantity)
   end
 
   def add_to_cart(cart, quantity)
     cart_product = cart.cart_products.find_by(product: self)
     if cart_product.present?
+      # If the product already exists in the cart, increase the quantity
       cart_product.update!(quantity: cart_product.quantity + quantity)
     else
-      cart_product = cart.cart_products.create!(product: self, quantity: quantity)
+      # If the product does not exist in the cart, create a new cart product
+      cart.cart_products.create!(product: self, quantity: quantity)
     end
+    # If the product is added to the cart, reduce the stock
     decrease_stock!(quantity)
   end
 end
